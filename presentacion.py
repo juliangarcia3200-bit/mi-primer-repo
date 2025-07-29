2c0416b33928457bfd0763fe96c0c9c2f32d719e
juliangarcia3200 <juliangarcia3200@gmail.com>
2025-07-29 18:34:58 -0500

    Agrego funcion y entrada de color favorito

--- presentacion.py
+++ presentacion.py
@@ -2,3 +2,11 @@ nombre = "Julian"
 edad = 24
 lenguaje = "Python"
 print(f"Hola, mi nombre es {nombre}, tengo {edad} años y estoy aprendiendo {lenguaje}. Hola estoy modificando")
+
+def comida_favorita():
+    comida = "Pizza"
+    print(f"Mi comida favorita es {comida}.")
+
+comida_favorita()
+color = input("¿Cuál es tu color favorito? ")
+print(f"Tu color favorito es {color}.")
